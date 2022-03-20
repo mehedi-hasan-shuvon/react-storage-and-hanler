daed71e30697912003995561e6d46085c01d6fcf
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-03-21 01:11:48 +0600

    learn export and import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Cosmatics from './components/Cosmatics/Cosmatics';
+import Shoes from './components/Shoes/Shoes';
 
 function App() {
   return (
     <div className="App">
       <Cosmatics></Cosmatics>
+      <Shoes></Shoes>
     </div>
   );
 }
