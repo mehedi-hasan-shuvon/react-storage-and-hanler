b1303249fe69ad58bc4dd7bb977fc30245773258
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-03-21 11:29:33 +0600

    learn event handler

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import './Cosmetic.css'
 const Cosmetic = (props) => {
+
+
+    const addtoCart = (id) => {
+        console.log("item added", id);
+    };
+
+    const addToCartWithParam = () => {
+        addtoCart(props.cosmetic.id);
+    };
     return (
-        <div>
+        <div className='product'>
             <h2>Buy This: {props.cosmetic.name}</h2>
             <p>Only for: {props.cosmetic.price}$</p>
+
+            <button onClick={addToCartWithParam}>Add to cart</button>
+            <button onClick={() => addtoCart(props.cosmetic.id)}>Add to cart shortcut</button>
         </div>
     );
 };
